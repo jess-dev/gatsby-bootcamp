5c533e9cb1d2527fc658b115c2749ec4f7a9a68d
Jess Taylor <jessica.taylor@travelsphere.co.uk>
2020-01-30 23:58:03 +0000

    Addition of blog posts

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
                 <h1>Contact Me</h1>
                 <h3>Get in Touch via:</h3>
                 <ul>
-                    <li><a href="https://twitter.com/jessicaisace" target="_blank">@jessicaisace</a></li>
+                    <li><a href="https://twitter.com/jessicaisace" target="_blank" rel="noopener noreferrer">@jessicaisace</a></li>
                     <li>Email</li>
                     <li>LinkedIn</li>
                 </ul>
